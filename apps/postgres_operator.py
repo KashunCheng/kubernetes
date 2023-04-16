a90601020b513471cccbc47fe9da200f8584dc1e
KashunCheng <kashun@berkeley.edu>
2023-04-16 14:28:53 -0700

    feat: postgres operator backups

--- apps/postgres_operator.py
+++ apps/postgres_operator.py
@@ -1,10 +1,20 @@
 from transpire import helm
+from transpire.resources import Secret
 from transpire.utils import get_versions
 
 name = "postgres-operator"
 
 
 def objects():
+    s3_secret = "storage-s3-key"
+    yield Secret(
+        name=s3_secret,
+        string_data={
+            "STORAGE_S3_ACCESS_KEY_ID": "",
+            "STORAGE_S3_SECRET_ACCESS_KEY": "",
+        },
+    ).build()
+
     yield from helm.build_chart_from_versions(
         name=name,
         versions=get_versions(__file__),
@@ -28,5 +38,33 @@ def objects():
             },
             "numberOfInstances": 1,
             "postgresql": {"version": "14"},
+            # https://thedatabaseme.de/2022/03/26/backup-to-s3-configure-zalando-postgres-operator-backup-with-wal-g/
+            "env": list(map(lambda pair: {"name": pair[0], "value": pair[1]}, {
+                "WAL_S3_BUCKET": "ocf-postgres-backup",
+                "WAL_BUCKET_SCOPE_PREFIX": "",
+                "WAL_BUCKET_SCOPE_SUFFIX": "",
+                "USE_WALG_BACKUP": "true",
+                "USE_WALG_RESTORE": "true",
+                'BACKUP_SCHEDULE': '00 10 * * *',
+                "AWS_S3_FORCE_PATH_STYLE": "true",
+                "AWS_ENDPOINT": "https://o3.ocf.berkeley.edu",
+                "AWS_REGION": "us-east-1",
+                "WALG_DISABLE_S3_SSE": "false",
+                "BACKUP_NUM_TO_RETAIN": "5",
+                "CLONE_USE_WALG_RESTORE": "true"
+            }.items())) + [
+                       {"name": "AWS_ACCESS_KEY_ID", "valueFrom": {
+                           "secretKeyRef": {
+                               "name": s3_secret,
+                               "key": "STORAGE_S3_ACCESS_KEY_ID"
+                           }
+                       }},
+                       {"name": "AWS_SECRET_ACCESS_KEY", "valueFrom": {
+                           "secretKeyRef": {
+                               "name": s3_secret,
+                               "key": "STORAGE_S3_SECRET_ACCESS_KEY"
+                           }
+                       }}
+                   ]
         },
     }
